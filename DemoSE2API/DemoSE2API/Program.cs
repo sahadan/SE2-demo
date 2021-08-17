c6f9a00580845b8e65bd348e5e692b91ee2d1529
sahadan <sahadan@gmail.com>
2021-08-17 10:50:01 +0530

    Second commit

--- DemoSE2API/DemoSE2API/Program.cs
+++ DemoSE2API/DemoSE2API/Program.cs
@@ -14,6 +14,7 @@ namespace DemoSE2API
     {
         public static void Main(string[] args)
         {
+            //Entry point
             CreateWebHostBuilder(args).Build().Run();
         }
 
